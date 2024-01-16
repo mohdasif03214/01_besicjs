c2a5b1892acf63e209b4b944a1909330ba5d3054
mohdasif03214 <141476866+mohdasif03214@users.noreply.github.com>
2024-01-16 15:46:48 +0000

    variables

---
+++ 01_basic/veriable.js
@@ -0,0 +1,17 @@
+const accountId = 123456;
+let accountEmail = "asif@123gmail.com";
+var accountPassword = "15689";
+accountCity = "lakhimpur";
+// accountId=456;
+accountEmail = "ansari@34gmail.com";
+accountPassword = 963;
+let accountState = "uttar pradesh";
+
+//console.log(accountPassword);
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
