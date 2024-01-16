732df9a46f92c236397b03387c650fec8977ccbd
mohdasif03214 <141476866+mohdasif03214@users.noreply.github.com>
2024-01-16 15:46:00 +0000

    oprator

---
+++ 01_basic/oprator.js
@@ -0,0 +1,18 @@
+let a=5;
+let b=a++;
+console.log(b);
+
+let x=10;
+let y=++x;
+let z= ++y;
+console.log(z);
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
+let d = 3;
+let e = ++d
+let f = ++e
+console.log(f,e);
