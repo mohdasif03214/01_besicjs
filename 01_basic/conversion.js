2000806729057b0a76a88b2f4ada7aa432f55a3f
mohdasif03214 <141476866+mohdasif03214@users.noreply.github.com>
2024-01-16 15:41:52 +0000

    learn with coding

---
+++ 01_basic/conversion.js
@@ -0,0 +1,58 @@
+/*let score = "33";
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);*/
+ 
+
+/*let score = null;
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(valueInNumber);*/
+
+
+/*let score = true;
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(valueInNumber);*/
+
+
+
+// "33"=> 33
+// "331qwe"=> NaN
+// true => 1; false=> 0
+
+
+/*let isLoggedIn = "1";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);*/
+
+/*let someNumber = 50;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);*/
+
+
+
+/*let price = 45;
+console.log(price);
+console.log(typeof price);
+
+let stringNumber = String(price);
+console.log(stringNumber);
+console.log(typeof stringNumber);*/
+
+
+// let price = "true";
+// console.log(price);
+
+// let valueInNumber = Number(price);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+let someNumber = true;
+console.log(someNumber);
+
+let stringNumber =String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
