8faf8ae6573e98c9f18a7fd21ae1f6a962cf64cf
mohdasif03214 <141476866+mohdasif03214@users.noreply.github.com>
2024-01-16 15:44:42 +0000

    data type

---
+++ 01_basic/datatype.js
@@ -0,0 +1,11 @@
+let a = "asif"
+let b = true;
+let c = 125;
+let d = null;
+// let e = bigInt("63254");
+let f = Symbol("i am asif");
+let g;
+console.log(b);
+console.table([a,b,c,d,f,g]);
+console.table(typeof a);
+  
